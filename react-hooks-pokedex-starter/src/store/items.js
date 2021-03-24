64905837c6ed751735d8ecafc3dc53d59d2a4e42
Marc Carlson <marc.carlson30@gmail.com>
2021-03-24 15:35:06 -0500

    completed phase 5

--- react-hooks-pokedex-starter/src/store/items.js
+++ react-hooks-pokedex-starter/src/store/items.js
@@ -25,6 +25,14 @@ const remove = (itemId, pokemonId) => ({
   pokemonId,
 });
 
+export const getItems = (id) => async dispatch => {
+  const response = await fetch(`/api/pokemon/${id}/items`)
+  if (response.ok) {
+    const items = await response.json()
+    dispatch(load(items, id))
+  }
+}
+
 const initialState = {};
 
 const itemsReducer = (state = initialState, action) => {
